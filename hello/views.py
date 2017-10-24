270ff44350b2d4b17332fe3c3a8f1bfd3a75d2c7
Adam Tak <adam.tak2112@gmail.com>
2017-10-24 19:55:33 +0000

    fixed views.py to include timestamp

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+import time
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello index")
+	return HttpResponse("The time over here is: " + time.strftime("%c"))
