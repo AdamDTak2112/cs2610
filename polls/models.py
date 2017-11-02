465796884ebf651ab734b9168b14196d8fb0fe48
Adam Tak <adam.tak2112@gmail.com>
2017-11-02 18:15:16 +0000

    corrected models on polls

--- polls/models.py
+++ polls/models.py
@@ -3,9 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
