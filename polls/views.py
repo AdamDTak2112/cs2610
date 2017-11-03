104887f1db042b3be8359ee78fa3a31d0dc5d856
Adam Tak <adam.tak2112@gmail.com>
2017-11-03 05:33:46 +0000

    revision

--- polls/views.py
+++ polls/views.py
@@ -9,9 +9,7 @@ from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
+    context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
