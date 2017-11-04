4a4083fc05844b3aa851b5f64ac1fc3518a44676
Adam Tak <adam.tak2112@gmail.com>
2017-11-04 01:03:21 +0000

    fixed url structure to default to polls app

--- cs2610/urls.py
+++ cs2610/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('hello.urls')),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
