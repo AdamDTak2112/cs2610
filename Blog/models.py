31f6b65284099824dbd3f6241d5f95f76fe68fc2
Adam Tak <adam.tak2112@gmail.com>
2017-11-22 00:10:48 +0000

    added blog and comment models

--- Blog/models.py
+++ Blog/models.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    author_name = models.CharField(max_length=30)
+    content = models.TextField()
+    post_date = models.DateField('Date posted')
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE,)
+    commenter_nickname = models.CharField(max_length=30)
+    email_address = models.CharField(max_length=30)
+    content = models.TextField()
+    post_date = models.DateTimeField('Date posted')
