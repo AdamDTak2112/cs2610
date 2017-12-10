3a83a25f1ee23af52dd3411742c5ff2f3ab38775
Adam Tak <adam.tak2112@gmail.com>
2017-12-10 22:21:21 +0000

    implemented conversion views and models for conversion

--- ConvertUnit/models.py
+++ ConvertUnit/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Convert(models.Model):
+    factors = {'g': 1, 't_oz': 31.10348, 'lbs': 453.5924, 'kg': 1000, 'ton': 1000000, 'oz': 28.34952}
+    def __str__(self):
+        return self.factors
--- ConvertUnit/views.py
+++ ConvertUnit/views.py
@@ -2,5 +2,31 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Convert
+import json
 # Create your views here.
+
+def convert(value, to, fro):
+    newValue = (value / Convert.factors[fro]) * Convert.factors[to]
+    return newValue
+
+def convertFromRequest(request):
+    resp = {};
+    if not request.GET.has_key('value' and 'from' and 'to'):
+        resp['error'] = "Not a valid conversion factor"
+    else:
+        value = int(request.GET['value'])
+        to = request.GET['to']
+        fro = request.GET['from']
+        if (value < 0):
+            resp['error'] = "Not a valid number to convert"
+        else:
+           val = convert(value, to, fro)
+           resp = { 'units': to, 'value': val }
+           
+    return HttpResponse(json.dumps(resp))
+    
+def index(request):
+    return render(request, '')
+            
